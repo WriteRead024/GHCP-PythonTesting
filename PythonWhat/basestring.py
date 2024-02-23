f3c56d4b3e514257a34a538ee3136a862698cabf
WriteRead024 <rich@rwhifonline.net>
2024-02-23 18:08:17 -0500

    basestring definition check

---
+++ PythonWhat/basestring.py
@@ -0,0 +1,19 @@
+import sys
+
+# Print Python version
+print()
+print("current Python version:", sys.version)
+
+# Check if basestring is defined
+print()
+print("checking if basestring is defined in the current environment globals()")
+if 'basestring' in globals():
+    print("basestring is defined, ")
+    print("which is expected in Python 2.x")
+else:
+    print("basestring is not defined.")
+    print("(expected in Python 3.x)")
+
+print()
+print("The purpose of the basestring class was to allow a single test to check for both str and unicode.")
+print()
