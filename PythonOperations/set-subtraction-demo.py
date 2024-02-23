23aef953b3a11f9ec7bfc5884b789b31a0b8d8fb
WriteRead024 <rich@rwhifonline.net>
2024-02-23 14:58:05 -0500

    added explanation to demo script

--- PythonOperations/set-subtraction-demo.py
+++ PythonOperations/set-subtraction-demo.py
@@ -4,13 +4,26 @@
 # with Rich W.
 # MSL.l
 
-# demo of set subtraction operation
+print()
+print("demo of set subtraction operation")
+print()
 
 set1 = {"apple", "banana", "orange", "grape"}
 set2 = {"banana", "grape", "kiwi", "mango"}
 
-result1 = set1 - set2
-result2 = set2 - set1
+print("elements of set1:", set1)
+print("elements of set2:", set2)
 
+print()
+
+print("Subtracting set2 from set1")
+result1 = set1 - set2
 print("Elements in set1 but not in set2:", result1)
+
+print()
+
+print("Subtracting set1 from set2")
+result2 = set2 - set1
 print("Elements in set2 but not in set1:", result2)
+
+print()
