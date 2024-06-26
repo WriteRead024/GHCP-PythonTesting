e9f66b7e061c79130e4c62924de3a7ba1d6ba159
WriteRead024 <rich@rwhifonline.net>
2024-06-26 14:33:20 -0400

    fix fail from file rename

--- PythonUnitTesting/test_template-filename.py
+++ PythonUnitTesting/test_template-filename.py
@@ -5,7 +5,7 @@ import unittest
 class TemplateFile(unittest.TestCase):
     def test_path_basename(self):
         filename = path.basename(__file__)
-        self.assertEqual(filename, "test_template_filename.py")
+        self.assertEqual(filename, "test_template-filename.py")
 
 if __name__ == "__main__":
     unittest.main()
